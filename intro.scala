3f25e1c2294b8a688a0032d2c4b2c057847bfd0d
charity <wa.nelimacharity@gmail.com>
2023-08-30 17:34:32 +0300

    scala ex

--- intro.scala
+++ intro.scala
@@ -1,2 +1,6 @@
 The name Scala stands for “scalable language.” The language is so named
 because it was designed to grow with the demands of its users. 
+
+var capital = Map("US" -> "Washington", "France" -> "Paris")
+capital += ("Japan" -> "Tokyo")
+println(capital("France"))
